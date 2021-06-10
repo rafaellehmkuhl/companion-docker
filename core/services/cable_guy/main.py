23ff8f52dad6e8bc1b91d87146052ae0ac667418
Rafael Araujo Lehmkuhl <rafael.lehmkuhl93@gmail.com>
2021-06-10 16:58:27 -0300

    core: cable-guy: Add proper logging

--- core/services/cable_guy/main.py
+++ core/services/cable_guy/main.py
@@ -1,5 +1,7 @@
 #! /usr/bin/env python3
+import argparse
 import json
+import logging
 import os
 import sys
 from pathlib import Path
@@ -9,10 +11,40 @@ import uvicorn
 from fastapi import Body, FastAPI, HTTPException, status
 from fastapi.staticfiles import StaticFiles
 from fastapi_versioning import VersionedFastAPI, version
+from loguru import logger
 from starlette.responses import Response as StarletteResponse
 
 from api import manager
 from api.manager import EthernetInterface
+
+parser = argparse.ArgumentParser(description="Cable Guy service for Blue Robotics Companion")
+parser.add_argument("-v", "--verbose", help="increase output verbosity", action="store_true")
+
+args = parser.parse_args()
+
+if not args.verbose:
+    logger.remove()
+    logger.add(sys.stderr, format="<green>{time:HH:mm:ss.SSS}</green> | <level>{message}</level>")
+
+
+class InterceptHandler(logging.Handler):
+    def emit(self, record) -> None:  # type: ignore
+        # Get corresponding Loguru level if it exists
+        try:
+            level = logger.level(record.levelname).name
+        except ValueError:
+            level = record.levelno
+
+        # Find caller from where originated the logged message
+        frame, depth = logging.currentframe(), 2
+        while frame.f_code.co_filename == logging.__file__:
+            frame = frame.f_back  # type: ignore
+            depth += 1
+
+        logger.opt(depth=depth, exception=record.exc_info).log(level, record.getMessage())
+
+
+logging.basicConfig(handlers=[InterceptHandler()], level=0)
 
 
 class PrettyJSONResponse(StarletteResponse):
@@ -34,6 +66,7 @@ app = FastAPI(
     title="Cable Guy API",
     description="Cable Guy is responsible for managing internet interfaces on Companion.",
     default_response_class=PrettyJSONResponse,
+    debug=True,
 )
 
 
@@ -68,7 +101,10 @@ app.mount("/", StaticFiles(directory=str(HTML_FOLDER), html=True))
 
 if __name__ == "__main__":
     if os.geteuid() != 0:
-        print("You need root privileges to run this script.\nPlease try again, this time using **sudo**. Exiting.")
+        logger.error(
+            "You need root privileges to run this script.\nPlease try again, this time using **sudo**. Exiting."
+        )
         sys.exit(1)
 
-    uvicorn.run(app, host="0.0.0.0", port=9090)
+    # Running uvicorn with log disabled so loguru can handle it
+    uvicorn.run(app, host="0.0.0.0", port=9090, log_config=None)
