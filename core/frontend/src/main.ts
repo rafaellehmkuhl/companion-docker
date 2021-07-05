b2120d27cd4a19bcc13c3309274dafa4be6a5231
Rafael Araujo Lehmkuhl <rafael.lehmkuhl93@gmail.com>
2021-07-05 16:34:04 -0300

    core: frontend: Add vuex-module-decorators

--- core/frontend/src/main.ts
+++ core/frontend/src/main.ts
@@ -3,6 +3,10 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
+import { config } from 'vuex-module-decorators'
+
+// Set rawError to true by default on all @Action decorators
+config.rawError = true
 
 new Vue({
   router,
